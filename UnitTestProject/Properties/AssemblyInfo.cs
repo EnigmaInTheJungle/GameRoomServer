b8b0acb3ca44736918bba205a1c4b81b65a53a1d
Надежда Бердник <nadya.berdnik@mail.ru>
2017-12-06 00:58:31 +0200

    Add UT. Not working yet

---
+++ UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("84da2d63-07fa-4cd7-80bb-d3d16371cd13")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject/UnitTestAuthReg.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GameRoomsAPI;
+using System.Threading;
+using Moq;
+using GameRoomsAPI.Controllers;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTestAuthReg
+    {
+        [TestMethod]
+        public void TestCreateUser()
+        {
+            var userStore = new UserStore<IdentityUser>();
+            var manager = new UserManager<IdentityUser>(userStore);
+            var user = new IdentityUser() { UserName = "NewUser" };
+            IdentityResult result = manager.Create(user, "123abc");
+            Assert.AreEqual(result, IdentityResult.Success);
+        }
+
+        [TestMethod]
+        public void TestLoginUser()
+        {
+            var userStore = new UserStore<IdentityUser>();
+            var manager = new UserManager<IdentityUser>(userStore);
+            var user = new IdentityUser() { UserName = "NewUser" };
+            IdentityResult result = manager.Create(user, "123abc");
+            
+            var userManager = new UserManager<IdentityUser>(userStore);
+            var userLogin = userManager.Find("NewUser", "123abc");
+            Assert.AreNotEqual(null, userLogin);
+
+        }
+    }
+}
