5842d8faa74372ed08c1ab289afabdf9f2aca55c
Надежда Бердник <nadya.berdnik@mail.ru>
2017-12-07 17:50:53 +0200

    Started tests

---
+++ UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d6e34a05-245f-4820-95ff-aa2e376db5d8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject/UnitTestsUsersSocket.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GameRoomsAPI.WebSockets;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTestsUsersSocket
+    {
+        WebSocketServer wssv = null;
+        [TestInitialize]
+        public void StartServer()
+        {
+            wssv = new WebSocketServer(4649);
+            wssv.AddWebSocketService<UsersSocket>("/UsersSocket");
+            wssv.Start();
+        }
+
+        [TestCleanup]
+        public void ServerStop()
+        {
+            wssv.Stop();
+        }
+
+        [TestMethod]
+        public void TestWebSocketWithServiceIsListening()
+        {
+            Assert.AreEqual(true, wssv.IsListening);
+        }
+
+        [TestMethod]
+        public void TestMethodWebSocketWithService()
+        {
+            var us = new UsersSocket();
+            var ws = new WebSocket("ws://localhost:4649");
+            ws.OnOpen += (sender, e) => ws.Send("Hi, there!");
+            ws.Connect();
+            
+            Assert.AreEqual(true, wssv.IsListening);
+        }
+    }
+}
